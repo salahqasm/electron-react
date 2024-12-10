e57c618262c2bd4ed70ade9ad4fafe732811a89b
Salah Al Deen Khalaf <salah.al.deen.khalaf@pwc.com>
2024-12-10 17:48:05 +0300

    DX improvments

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,13 +1,17 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
   });
-
-  win.loadFile(path.join(app.getAppPath(), "/dist-ui/index.html"));
+  if (isDev()) {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(path.join(app.getAppPath(), "/dist-ui/index.html"));
+  }
 };
 
 app.whenReady().then(() => {
